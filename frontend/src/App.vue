819f7f57e22e2714fd69fa26b48ba7d3c6af6d6f
CM <christofferjcm@gmail.com>
2021-02-10 14:06:25 +0100

    single header in app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
-    <router-view />
+    <Header />
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
-export default {};
+import Header from "@/components/Header";
+
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+};
 </script>
 
 <style lang="scss">
 @import "@/styles/_global.scss";
 
 #app {
-  display: flex;
-  justify-content: center;
+  max-width: 100rem;
+  margin: 0 auto;
+}
+
+body {
   background-color: lightgray;
 }
 </style>
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,38 +1,66 @@
 <template>
   <header>
-    <img id="logo" src="@/assets/sinus-logo.svg" alt="" @click="routeToStart" />
-    <div class="header-links">
-      <router-link to="/"> <button>Home</button> </router-link>
-      <router-link to="/products"><button>Products</button> </router-link>
-      <button class="login-btn"></button>
+    <div class="navbar">
+      <img id="logo" src="@/assets/sinus-logo.svg" alt @click="routeToStart" />
+      <div class="header-links">
+        <router-link to="/products">
+          <button>Products</button>
+        </router-link>
+        <button class="login-btn"></button>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
 export default {
+  name: "Header",
   methods: {
     routeToStart() {
       this.$router.push(`/`);
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 @import "@/styles/_global.scss";
 
-#logo {
-  height: 5rem;
-  cursor: pointer;
-}
-header {
+.navbar {
+  width: 90%;
   display: flex !important;
   align-items: center;
-  justify-content: space-between;
+  margin: 1.5rem auto;
 }
-.login-btn {
-  @include lock-button;
-  margin: 5px;
+
+#logo {
+  height: auto;
+  max-height: 4rem;
+  cursor: pointer;
+  margin: 0.1rem;
+  margin-right: auto;
+}
+
+.header-links {
+  padding: 0.5rem 0 0 0;
+
+  button {
+    font-weight: bold;
+    font-size: 16px;
+    font-family: inherit;
+    margin: 0 0.5rem;
+    border: none;
+    background-color: transparent;
+  }
+
+  button:hover {
+    color: white;
+  }
+  .login-btn {
+    @include lock-button;
+    margin-bottom: 0.1rem;
+    background-color: white;
+    vertical-align: middle;
+  }
 }
 </style>
