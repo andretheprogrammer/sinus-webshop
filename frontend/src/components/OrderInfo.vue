ead97d4983c57d2822e7f459b9bb9acf42d3ff02
andretheprogrammer <andre.lundstrom@gmail.com>
2021-02-16 17:36:49 +0100

    chosenorder fixes

--- frontend/src/components/OrderInfo.vue
+++ frontend/src/components/OrderInfo.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="wrapper">
-    <h1>{{ chosenOrder }}</h1>
+    <h1>{{ orderItems.title }}</h1>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 export default {
+  data() {
+    return {
+      orderItems: [],
+    };
+  },
   computed: {
-    ...mapGetters(["chosenOrder", "orderResponse"]),
+    ...mapGetters(["chosenOrder", "orderResponse", "productResponse"]),
+    fillOrderItems() {
+      return this.productResponse.filter((item) => {
+        return this.chosenOrder.items.includes(item._id);
+      });
+    },
+  },
+  mounted() {
+    this.orderItems = this.filteredEntrys;
   },
 };
 </script>
