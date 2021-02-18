9ab516007030814155fb9d80cc137676fdcf577a
andretheprogrammer <andre.lundstrom@gmail.com>
2021-02-18 20:51:50 +0100

    changed my mind

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import LottieAnimation from 'lottie-vuejs'
 
-Vue.use(LottieAnimation)
 
 Vue.config.productionTip = false
 
